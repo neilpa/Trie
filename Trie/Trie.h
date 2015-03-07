22b96c7b27f3aab26436199279e7265ebd564af2
Neil Pankey <npankey@gmail.com>
2015-03-06 17:28:26 -0800

    Initial commit

---
+++ Trie/Trie.h
@@ -0,0 +1,8 @@
+//  Copyright (c) 2015 Neil Pankey. All rights reserved.
+
+/// Project version number for Trie.
+extern double TrieVersionNumber;
+
+/// Project version string for Trie.
+extern const unsigned char TrieVersionString[];
+
